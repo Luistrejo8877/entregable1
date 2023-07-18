f646066ae65c7ac80dc6c84fd39281ad095434eb
Luis Trejo <134348738+Luistrejo8877@users.noreply.github.com>
2023-07-18 00:10:52 -0700

    hfjsahjkfs

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import UserCard from './components/UserCard'
+import phrases from './data/phrases.json'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <UserCard />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,18 @@
+
+const UserCard = ({data}) => {
+
+   
+  
+    return (
+        <article>
+            <h1>Galleta de la fortuna</h1>
+            <h2>{data.phrase} {data.author}</h2>
+            
+            <div>
+
+            </div>
+        </article>
+    )    
+}
+
+export default UserCard
