18208bc38c29df12a3212dd7f013357bce047f91
Luis Trejo <134348738+Luistrejo8877@users.noreply.github.com>
2023-08-11 13:33:41 -0700

    entregablegalleta

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -5,10 +5,10 @@ const UserCard = ({data}) => {
     return (
         <div className="main" >
             <div className="cardPhrase">
-            <p className="phrase">{data.phrase}</p>
+                <p className="phrase">{data.phrase}</p>
             </div>
-           
         </div>
     )
 }
-export default UserCard
+export default UserCard           
+
